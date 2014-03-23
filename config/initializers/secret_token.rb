d0c74d80a0011357fd674b58dffa543678f67355
anvithaj90 <anvithaj90@gmail.com>
2014-03-22 17:34:50 -0700

    Initialized repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AssistShutterbug::Application.config.secret_key_base = '41a2a13290214ee7beb8a114bbec1d595e6d5ee49144e7f07b4ea9e595931f491c1106ca3ea5eec49b875812aaecea4b1df0459f1f52a38915db0ee533d80c24'
